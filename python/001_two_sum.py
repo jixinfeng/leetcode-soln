95587bcb6c9f23862e7733d931b2500368a32b3e
Jixin Feng <ufjfeng@users.noreply.github.com>
2016-09-29 16:20:38 -0400

    reduce complexity of 001

--- python/001_two_sum.py
+++ python/001_two_sum.py
@@ -22,7 +22,19 @@ class Solution(object):
         :rtype: List[int]
         """
         l=len(nums)
+        numsLoc = collections.defaultdict(list)
+        for i, j in enumerate(nums):
+            numsLoc[j].append(i)
         for i in range(l):
-            for j in range(i+1,l):
-                if nums[i]+nums[j]==target:
-                    return [i,j]
+            num1 = nums[i]
+            num2 = target - num1
+            if num2 in numsLoc:
+                for j in numsLoc[num2]:
+                    if j > i:
+                        return [i, j]
+                        
+import collections
+a = Solution()
+print(a.twoSum([2, 7, 11, 15], 9) == [0, 1])
+print(a.twoSum([3, 2, 4], 6) == [1, 2])
+print(a.twoSum([0, 4, 3, 0], 0) == [0, 3])
