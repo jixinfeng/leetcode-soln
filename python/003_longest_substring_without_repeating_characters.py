833b4dce9cc9f3fd0e3e7ced3492288356f4fa41
Jixin Feng <ufjfeng@users.noreply.github.com>
2016-09-30 20:11:03 -0400

    fix 003

--- python/003_longest_substring_without_repeating_characters.py
+++ python/003_longest_substring_without_repeating_characters.py
@@ -11,31 +11,7 @@ Examples:
     Given "pwwkew", the answer is "wke", with the length of 3. Note that the
     answer must be a substring, "pwke" is a subsequence and not a substring.
 
-First try: works but too slow
-    def lengthOfLongestSubstring(self, s):
-        l=len(s)
-        if l==0:
-            return str()
-        elif l==1:
-            return s
-        string=list(s)
-        maxstring=[]
-        skip=0
-        for i in range(l):
-            if i<skip:
-                continue
-            window=[string[i]]
-            for j in range(i+1,l):
-                if string[j] not in window:
-                    window.append(string[j])
-                else:
-                    if len(window)>len(maxstring):
-                        maxstring=window
-                    skip=window.index(string[j])+1
-                    break
-        return len(maxstring)
-
-Note:
+Hint:
     Need the length of longest string only, don't need the string
 """
 class Solution(object):
@@ -44,17 +20,15 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        l=len(s)
-        if l==0:
-            return 0
-        elif l==1:
-            return 1
-        lastCharPos={}
-        maxStrLen=0
-        maxStrStart=0
-        for i,ch in enumerate(s):
-            if ch in lastCharPos and lastCharPos[ch]>=maxStrStart:
-                maxStrStart=lastCharPos[ch]+1
-            lastCharPos[ch]=i
-            maxStrLen=max(maxStrLen,i-maxStrStart)
+        l = len(s)
+        if l < 2:
+            return l
+        lastCharPos = {}
+        maxStrLen = 0
+        maxStrStart = 0
+        for i, ch in enumerate(s):
+            if ch in lastCharPos and lastCharPos[ch] >= maxStrStart:
+                maxStrStart = lastCharPos[ch] + 1
+            lastCharPos[ch] = i
+            maxStrLen = max(maxStrLen, i - maxStrStart + 1)
         return maxStrLen
