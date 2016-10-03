27e1f568ea1a6e7b0a2f33432ce5d89f8a4f704b
Jixin Feng <ufjfeng@users.noreply.github.com>
2016-10-02 23:16:28 -0400

    revise 008

--- python/008_string_to_integer_atoi.py
+++ python/008_string_to_integer_atoi.py
@@ -18,21 +18,28 @@ class Solution(object):
         :type str: str
         :rtype: int
         """
-        str=str.strip()
-        match=re.findall('^([\+-]?\d+).*$',str)
-        if match==[]:
-            num=0
+        str = str.strip()
+        match = re.findall('^([+-]?\d+).*$', str)
+        if match == []:
+            num = 0
         else:
-            num=int(match[0])
-        if num>=2**31:
-            num=(2**31)-1
-        elif num<-2**31:
-            num=-2**31
+            num = int(match[0])
+        if num >= 2 ** 31:
+            num = (2 ** 31) - 1
+        elif num <- 2 ** 31:
+            num =- 2 ** 31
         return num
 
 import re
 a=Solution()
-cases=['42','-42','3.14','3456 with words',\
-            'words with 1234','2147483648','-2147483648']
-for case in cases:
-    print('output of',"'",case,"'",'is',a.myAtoi(case))
+cases = ['42','-42','3.14','3456 with words', '+-2',
+       'words with 1234','2147483648','-2147483648']
+matches = [42, -42, 3, 3456, 0,
+           0, 2147483647, -2147483648]
+
+for i in range(len(cases)):
+    print('output of', "'", cases[i], "'", 'is', a.myAtoi(cases[i]))
+    if a.myAtoi(cases[i]) == matches[i]:
+        print('pass')
+    else:
+        print('not pass')
