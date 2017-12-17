872ef6118745352323c2537d796e637fd27e45ec
Jixin Feng <ufjfeng@users.noreply.github.com>
2017-12-17 18:03:18 -0500

    add 692

---
+++ python/692_top_k_frequent_words.py
@@ -0,0 +1,50 @@
+"""
+Given a non-empty list of words, return the k most frequent elements.
+
+Your answer should be sorted by frequency from highest to lowest. If two words
+have the same frequency, then the word with the lower alphabetical order comes
+first.
+
+Example 1:
+    Input: ["i", "love", "leetcode", "i", "love", "coding"], k = 2
+    Output: ["i", "love"]
+
+Explanation: "i" and "love" are the two most frequent words.
+    Note that "i" comes before "love" due to a lower alphabetical order.
+
+Example 2:
+    Input: ["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"], k = 4
+    Output: ["the", "is", "sunny", "day"]
+
+Explanation: "the", "is", "sunny" and "day" are the four most frequent words,
+with the number of occurrence being 4, 3, 2 and 1 respectively.
+
+Note:
+You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+Input words contain only lowercase letters.
+
+Follow up:
+Try to solve it in O(n log k) time and O(n) extra space.
+"""
+class Solution(object):
+    def topKFrequent(self, words, k):
+        """
+        :type words: List[str]
+        :type k: int
+        :rtype: List[str]
+        """
+        if not words or not k:
+            return []
+        if len(words) < k:
+            return []
+        count = {}
+        for word in words:
+            count[word] = count.get(word, 0) + 1
+        freq = []
+        heapq.heapify(freq)
+        for w, c in count.items():
+            heapq.heappush(freq, (-c, w))
+        soln = []
+        for i in range(k):
+            soln.append(heapq.heappop(freq)[1])
+        return soln
