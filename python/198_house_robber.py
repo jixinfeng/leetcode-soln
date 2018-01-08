9557a9b2dceb08c01d9784bd1ebd2eaf0ee88fe3
Jixin Feng <ufjfeng@users.noreply.github.com>
2018-01-08 14:56:54 -0500

    update 198

--- python/198_house_robber.py
+++ python/198_house_robber.py
@@ -1,25 +1,19 @@
+"""
+You are a professional robber planning to rob houses along a street. Each house
+has a certain amount of money stashed, the only constraint stopping you from
+robbing each of them is that adjacent houses have security system connected and
+it will automatically contact the police if two adjacent houses were broken
+into on the same night.
+
+Given a list of non-negative integers representing the amount of money of each
+house, determine the maximum amount of money you can rob tonight without
+alerting the police.
+"""
 class Solution(object):
     def rob(self, nums):
         """
         :type nums: List[int]
         :rtype: int
-
-        Note:
-        Dynamic Programming
-        
-        dp[i]=max(dp[i-2]+num[i],dp[i-1])
-        dp[0]=nums[0]
-        dp[1]=max(nums[0],nums[1])
-
-    	Shorter Approach:
-        a=0
-        b=0
-        for num in nums:
-            m=a
-            n=b
-            a=n+num
-            b=max(m,n)
-        return max(a,b)
         """
         l = len(nums)
         if l == 0:
